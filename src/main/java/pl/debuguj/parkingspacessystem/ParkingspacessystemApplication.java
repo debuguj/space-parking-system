069bca187e85a0399f80537de7d411b9218a0c67
grzesiek <kontakt@debuguj.pl>
2017-10-07 14:15:27 +0200

    Initial commit

---
+++ src/main/java/pl/debuguj/parkingspacessystem/ParkingspacessystemApplication.java
@@ -0,0 +1,12 @@
+package pl.debuguj.parkingspacessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingspacessystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingspacessystemApplication.class, args);
+	}
+}
